ecc961d01ec9107ccdf6fd7112fe1ae584a36665
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2020-11-11 17:31:40 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("I am ready!");
-  client.user.setActivity("ur mom lol", { type: "WATCHING" });
+  client.user.setActivity("Hetalia: World Stars", { type: "WATCHING" });
 });
 
 // WELCOME MESSAGE WHEN USER HAS ACCEPTED THE RULES // 
