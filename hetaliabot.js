b439ec6296d049bf4baa5a3be05b5f9348e60a78
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2020-11-11 17:59:04 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -36,7 +36,7 @@ client.on("message", message => {
       let embed = new Discord.MessageEmbed()
       .setAuthor(`Welcome to the server, ${message.author.tag}`, message.author.displayAvatarURL())
       .setColor('#7ba3ff')
-      .setDescription(`<@${message.author.id}> just joined the discord! Make sure to fill out an <#423069581460897793> and get some <#759849308739731537> whenever!`)
+      .setDescription(`<@${message.author.id}> just joined the server! Make sure to fill out an <#423069581460897793> and get some <#759849308739731537> whenever!`)
       .setThumbnail(message.author.displayAvatarURL())
       .setTimestamp()
       .setFooter("PM PierreBot with any questions!")
